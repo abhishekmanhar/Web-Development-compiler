63d1a8be96d179f73b081ca047a1525e0a3decca
Abhishek Manhar <abhimanharkhan25@gmail.com>
2024-02-20 02:27:52 +0530

    client commit

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-full h-[calc(100dvh-60px)] text-white flex justify-center items-center flex-col gap-3">
+      <h1 className="text-6xl font-bold text-center">Web Development Compiler</h1>
+      <p className=" text-gray-500 text-center">
+        Code with HTML, CSS, JavaScript compiler and share it with your
+        friends!
+      </p>
+    </div>
+  );
+}
